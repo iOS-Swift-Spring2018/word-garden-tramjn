35ba8c86744f667301e6594a51a716341fa1a1dc
Tram Nguyen <tramnguyen@trams-mbp.bc.edu>
2018-02-05 15:42:22 -0500

    week 3 initial commit

--- Word Garden/ViewController.swift
+++ Word Garden/ViewController.swift
@@ -9,17 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var userGuessLabel: UILabel!
+    @IBOutlet weak var guessLetterField: UITextField!
+    @IBOutlet weak var guessLetterButton: UIButton!
+    @IBOutlet weak var guessCountLabel: UILabel!
+    @IBOutlet weak var playAgainButton: UIButton!
+    @IBOutlet weak var flowerImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        guessLetterButton.isEnabled = false
+        playAgainButton.isHidden = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func updateUIAfterGuess() {
+        guessLetterField.resignFirstResponder()
+        guessLetterField.text = ""
     }
+    @IBAction func guessLetterFieldChange(_ sender: UITextField) {
+        if let letterGuessed = guessLetterField.text!.last {
+            guessLetterField.text = "\(letterGuessed)"
+            guessLetterButton.isEnabled = true
+        } else {
+            // disable the button if i don't have a single char in the guessLetterField
+            guessLetterButton.isEnabled = false
+        }
+    }
+    
+    @IBAction func doneKeyPressed(_ sender: UITextField) {
+        updateUIAfterGuess()
 
+    }
+    
+    @IBAction func guessLetterButtonPressed(_ sender: UIButton) {
+        updateUIAfterGuess()
 
+    }
+    
+    @IBAction func playAgainButtonPressed(_ sender: UIButton) {
+    }
+    
 }
 
